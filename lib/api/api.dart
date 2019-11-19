4922c4ed70f6e89b121579c3062f30853f3a7799
lzh <lizh04@kaisagroup.com>
2019-11-19 19:40:58 +0800

    更新api地址

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -2,7 +2,7 @@ class Api {
   //static const String BASE_URL = 'http://192.168.190.84:8080/mall-app';
   //static const String BASE_URL = 'http://192.168.190.84:8080';
   //static const String BASE_URL='http://192.168.190.84:8082/wx';
-  static const String BASE_URL = 'http://luyouxin.xyz:8080/mall-app/wx';
+  static const String BASE_URL = 'http://120.25.226.11:8080//mall-app/wx';
   static const String HOME_URL=BASE_URL+'/home/index';//首页数据
   static const String BANNER_URL = BASE_URL + '/home/banner'; //获取首页banner图
   static const String CATEGORY_URL = BASE_URL + '/home/categories'; //获取首页分类
